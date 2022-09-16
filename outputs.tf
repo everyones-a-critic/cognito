18802d3f0dfa9072ac655a51b144584a5a323aa4
Daniel Long <24.daniel.long@gmail.com>
2022-09-16 18:21:04 -0500

    Adds a cognito pool id output

--- outputs.tf
+++ outputs.tf
@@ -3,6 +3,11 @@ output "congito_pool_arn" {
   value       = aws_cognito_user_pool.main.arn
 }
 
+output "congito_pool_id" {
+  description = "The ARN of the cognito user pool"
+  value       = aws_cognito_user_pool.main.arn
+}
+
 output "client_id" {
   description = "Client ID of the react native client created for interfacing with the user pool."
   value       = aws_cognito_user_pool_client.main.id
